098c12976c8ba77ba55db77d8f105674d9600828
Aryan <aryanbagale22@gmail.com>
2026-01-29 18:28:52 +0000

    Go

--- learn-gin/main.go
+++ learn-gin/main.go
@@ -6,17 +6,48 @@ import (
 )
 
 func main() {
-	// 1. Create a default router
-	r := gin.Default()
+	// --- STEP 1: ROUTER SETUP ---
+	
+	// Without Gin: 
+	// mux := http.NewServeMux() (You have to create a multiplexer manually)
+	
+	// With Gin:
+	r := gin.Default() // Creates router + Logger + Crash Recovery
 
-	// 2. Define a route
-	r.GET("/", func(c *gin.Context) {
+
+	// --- STEP 2: DEFINING A ROUTE ---
+
+	// Without Gin: 
+	// mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+	
+	// With Gin:
+	r.GET("/ping", func(c *gin.Context) {
+
+		// --- STEP 3: SENDING JSON ---
+
+		// Without Gin:
+		// w.Header().Set("Content-Type", "application/json")
+		// w.WriteHeader(http.StatusOK)
+		// json.NewEncoder(w).Encode(map[string]string{"message": "pong"})
+
+		// With Gin:
+		// (Handles Headers, Status Code, and Encoding in one line)
 		c.JSON(http.StatusOK, gin.H{
-			"message": "Build with Gin",
-			"status":  "success",
+			"message": "pong",
 		})
 	})
 
-	// 3. Start the server (defaults to port 8080)
-	r.Run() 
+	r.GET("/info", func(c *gin.Context){
+		c.JSON(http.StatusOK, gin.H{
+			"status":"active",
+		})
+	})
+
+	// --- STEP 4: STARTING SERVER ---
+
+	// Without Gin:
+	// http.ListenAndServe(":8080", mux)
+
+	// With Gin:
+	r.Run() // Defaults to :8080
 }
